a5862701cdb5c98d3564b310da7f8710edd1222b
yusufkaraasln <yusufkaraasln@gmail.com>
2023-12-20 18:54:24 +0300

    truva is ready

--- service/DiagnoService.js
+++ service/DiagnoService.js
@@ -3,6 +3,7 @@ import User from '../model/User.js';
 import dotenv from 'dotenv';
 import Prompter from '../config/prompter.js';
 import Result from '../dto/Result.js';
+import Truva from '../config/truva.js';
 
 dotenv.config();
 
@@ -24,13 +25,60 @@ export default class DiagnoService {
 
       const isValidPrompt = checkRequest.choices[0].message.content == 'true';
 
+      let data = null;
+
       if (isValidPrompt) {
-        // const emptyObject = {
-        //   disease_name: '',
-        //   disease_medicine_department: '',
-        //   why: '',
-        //   general_summary_and_advice: ''
-        // };
+        switch (closestPainArea) {
+          case 'abs':
+          case 'obliques':
+            data = Truva.belly();
+            break;
+
+          case 'chest':
+            data = Truva.chest();
+            break;
+
+          case 'lower-back':
+          case 'upper-back':
+            data = Truva.back();
+            break;
+
+          case 'biceps':
+          case 'triceps':
+          case 'forearm':
+            data = Truva.arm();
+            break;
+
+          case 'hands':
+            data = Truva.hand();
+            break;
+
+          case 'trapezius':
+          case 'deltoids':
+            data = Truva.shoulders();
+            break;
+
+          case 'head':
+          case 'neck':
+          case 'hair':
+            data = Truva.head();
+            break;
+
+          case 'ankles':
+          case 'hamstring':
+          case 'knees':
+          case 'quadriceps':
+          case 'calves':
+          case 'feet':
+          case 'adductors':
+          case 'gluteal':
+          case 'tibialis':
+            data = Truva.leg();
+            break;
+
+          default:
+            break;
+        }
 
         return {
           success: true,
